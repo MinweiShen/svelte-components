a5474e7728c73c303985d4b8803d04d998e30d54
沈旻蔚 <shenminwei@bytedance.com>
2021-11-19 13:22:47 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Carousel from "./components/carousel/Carousel.svelte";
+</script>
+
+<main>
+  <section class="carousel">
+    <Carousel />
+  </section>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  .carousel {
+    height: 100vh;
+  }
+</style>
---
+++ src/components/carousel/Carousel.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import type { ImageProps } from './types'
+  import p1 from '../../assets/p1.jpg'
+  import p2 from '../../assets/p2.jpg'
+  import p3 from '../../assets/p3.jpg'
+
+  export let images: Array<ImageProps|string> = [p1, p2, p3]
+  export let current = 0
+
+  function clickNav(i) {
+    current = i
+  }
+</script>
+
+
+
+<div class="carousel">
+  {#each images as image, i}
+    <img class={i === current ? '' : 'hidden'} src={typeof image === 'string' ? image : image.url} alt={typeof image === 'string' ? `carousel ${i}` : image.alt}>
+  {/each}
+  <div class="navs">
+    {#each images as _, i}
+      <div class="nav" on:click={() => clickNav(i)}></div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .carousel {
+    height: 100%;
+    width: 100%;
+    position: relative;
+    text-align: center;
+    overflow: hidden;
+  }
+
+  .navs {
+    position: absolute;
+    display: flex;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  
+  .nav {
+    cursor: pointer;
+    margin: 10px;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background-color: #ccc;
+  }
+
+  .nav:hover {
+    background-color: #aaa;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+    transition: 0.8s opacity;
+  }
+
+  img.hidden {
+    opacity: 0;
+    animation: fade-out 1.5s ease 1;
+  }
+
+  @keyframes fade-out {
+    100% {
+      transform: rotate(45deg) translateX(150%);
+    }
+  }
+</style>
